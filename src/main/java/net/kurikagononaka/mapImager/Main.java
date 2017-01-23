6e198124d466649b8216b2d2a5a660897ca261f0
Igaguri <igagurimk@gmail.com>
2017-01-23 20:54:19 +0900

    Add: library commons-cli

--- src/main/java/net/kurikagononaka/mapImager/Main.java
+++ src/main/java/net/kurikagononaka/mapImager/Main.java
@@ -15,6 +15,9 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
+
+
+
         try {
             List<InputStream> inputStreamList = new ArrayList<>();
 
